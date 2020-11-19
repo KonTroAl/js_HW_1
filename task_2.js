0afe841b376e952ab64c703bc751cd009c72b26b
KonTroAl <troshenkin.k@yandex.ru>
2020-11-19 20:46:29 +0500

    task 1 and task 2 completed

---
+++ task_2.js
@@ -0,0 +1,7 @@
+// 2.Объявить две переменные: admin и name. Записать в name строку "Василий";
+// Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).
+
+var admin
+var name = 'Konstantine'
+admin = name
+alert(admin)
